5e24b3de113b512a0190128dcbbb1fa9db0900e9
Andrey <andreytelushko@gmail.com>
2023-02-14 21:44:16 +0300

    Task

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введены массивы: 123, 23, hello, world, res");
+            Console.WriteLine("Масивы, длина  которых меньше либо равна 3 символа: ");
+            string[] array1 = new string[5] { "123", "23", "hello", "world", "res" };
+            string[] array2 = new string[array1.Length];
+            void SecondArrayWithIF(string[] array3, string[] array4)
+            {
+                int count = 0;
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    if (array1[i].Length <= 3)
+                    {
+                        array2[count] = array1[i];
+                        count++;
+                    }
+                }
+            }
+            void PrintArray(string[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+                Console.WriteLine();
+            }
+            SecondArrayWithIF(array1, array2);
+            PrintArray(array2);
         }
     }
 }
